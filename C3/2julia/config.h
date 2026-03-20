dd164457a752e99a286f07380faff843e8d187f5
favinom <favinom@USILU-16259MacProMarcoFavino.local>
2026-03-20 09:19:03 +0100

    reorganized folder

--- C3/2julia/config.h
+++ C3/2julia/config.h
@@ -1,10 +1,10 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define N 24001
-#define MAX_IT 10000
+#define N 2401
+#define MAX_IT 1000
 
-#define r2    4.0
+#define r2  4.0
 
 #define x0 -1.0
 #define y0 -1.0
@@ -17,4 +17,22 @@
 
 typedef double Real;
 
+//#define WRITE_TO_FILE
+
+#define TIC(name) \
+    struct timespec name##_t0, name##_t1; \
+    clock_gettime(CLOCK_MONOTONIC, &name##_t0)
+
+#define TOC(name, label) \
+    clock_gettime(CLOCK_MONOTONIC, &name##_t1); \
+    printf("%s: %.6f s\n", (label), \
+           (double)(name##_t1.tv_sec - name##_t0.tv_sec) + \
+           1e-9 * (double)(name##_t1.tv_nsec - name##_t0.tv_nsec))
+
+
 #endif
+
+//clock_t t0 = clock();
+//clock_t t1 = clock();
+//double elapsed = (double)(t1 - t0) / CLOCKS_PER_SEC;
+//printf("Loop time: %.6f s\n", elapsed);
--- C3/2julia/julia_c.c
+++ C3/2julia/julia_c.c
@@ -3,31 +3,7 @@
 #include <time.h>
 
 #include "config.h"
-
-int julia(Real re, Real im)
-{
-    int ret = MAX_IT+1;
-
-    Real re2 = re * re;
-    Real im2 = im * im;
-
-    for (int it = 0; it < MAX_IT; ++it)
-    {
-        im = 2.0 * re * im + c_im;
-        re = re2 - im2 + c_re;
-
-        re2 = re * re;
-        im2 = im * im;
-
-        if (re2 + im2 > r2)
-        {
-            ret = it;
-            break;
-        }
-    }
-    return ret;
-}
-
+#include "functions.h"
 
 int main(void)
 {
@@ -39,8 +15,7 @@ int main(void)
     const Real dx = Lx/(N-1);
     const Real dy = Ly/(N-1);
 
-    struct timespec t0, t1;
-    clock_gettime(CLOCK_MONOTONIC, &t0);
+    TIC(loop);
 
     for (int j = 0; j < N; ++j)
     {
@@ -55,32 +30,15 @@ int main(void)
         }
     }
 
-    clock_gettime(CLOCK_MONOTONIC, &t1);
+    TOC(loop,"Loop time");
 
-    double elapsed =
-        (double)(t1.tv_sec - t0.tv_sec) +
-        1e-9 * (double)(t1.tv_nsec - t0.tv_nsec);
+    #ifdef WRITE_TO_FILE
+    write_array_to_file("output_ser.txt", A, N * N);
+#else
+    printf("sum = %lld\n", sum_array(A, N * N));
+#endif
 
-    printf("Loop time: %.6f s\n", elapsed);
-
-	printf("%d\n",A[9240]);
-
-    FILE *fp = fopen("output_ser.txt", "w");
-    if (fp == NULL) {
-        perror("fopen");
-        return 1;
-    }
-
-    for (int i = 0; i < N*N; ++i)
-        fprintf(fp, "%d\n", A[i]);
 
     free(A);
     return 0;
 }
-
-
-       //clock_t t0 = clock();
-    //clock_t t1 = clock();
-
-    //double elapsed = (double)(t1 - t0) / CLOCKS_PER_SEC;
-    //printf("Loop time: %.6f s\n", elapsed);
