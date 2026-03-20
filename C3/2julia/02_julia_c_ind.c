01a9c02592b6466e2497d0a928681af8616492e7
favinom <favinom@USILU-16259MacProMarcoFavino.local>
2026-03-20 10:33:31 +0100

    original loop for ind version

--- C3/2julia/02_julia_c_ind.c
+++ C3/2julia/02_julia_c_ind.c
@@ -17,32 +17,32 @@ int main(void)
 
     TIC(loop);
 
-    int i = 0, j = 0;
-    for (int ind = 0; ind < N * N; ++ind)
+    //int i = 0, j = 0;
+    //for (int ind = 0; ind < N * N; ++ind)
+    //{
+    //    Real re = x0 + (Real)i * dx;
+    //    Real im = y0 + (Real)j * dy;
+    //
+    //    A[ind] = julia(re, im);
+
+    //   ++i;
+    //    if (i == N)
+    //    {
+    //        i = 0;
+    //        ++j;
+    //    }
+    //}
+
+    for (int ind = 0; ind < N*N; ++ind)
     {
-        Real re = x0 + (Real)i * dx;
-        Real im = y0 + (Real)j * dy;
+        int i=ind%N;
+        int j=ind/N;
+        
+        Real re = x0 + i * dx;
+        Real im = y0 + j * dy;
 
         A[ind] = julia(re, im);
-
-        ++i;
-        if (i == N)
-        {
-            i = 0;
-            ++j;
-        }
     }
-
-//    for (int ind = 0; ind < N*N; ++ind)
-//    {
-//        int i=ind%N;
-//        int j=ind/N;
-        
-//        Real re = x0 + i * dx;
-//        Real im = y0 + j * dy;
-
-//        A[ind] = julia(re, im);
-//    }
 
     TOC(loop,"Loop time");
 
